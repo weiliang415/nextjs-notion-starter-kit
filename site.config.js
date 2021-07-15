7b14a9a5e24338e0173f240d81bfce11ca8339fa
weiliang415 <74713438+weiliang415@users.noreply.github.com>
2021-07-15 21:54:06 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'eb5aba1358444737990abeb3b6c005ab',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
+  domain: 'https://panoramic-slip-396.notion.site/pili-eb5aba1358444737990abeb3b6c005ab',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
